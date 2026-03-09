4f98055a24214ac4fc1343b7e6dd2b67b958e943
arshan0999 <rangrezarshan3@gmail.com>
2026-03-09 18:05:12 +0530

    Fix CSV path and display revenue trend chart
    Updated the CSV file path and added a line to display the plotly chart.

--- dashboard/streamlit_app.py
+++ dashboard/streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 import plotly
 
-df = pd.read_csv("../data/coffee_sales.csv")
+df = pd.read_csv("data/coffee_sales.csv")
 
 df['revenue'] = df['transaction_qty'] * df['unit_price']
 df['transaction_time'] = pd.to_datetime(df['transaction_time'])
@@ -19,5 +19,6 @@ filtered_df = df[df['store_location'] == store]
 hourly_sales = filtered_df.groupby('hour')['revenue'].sum().reset_index()
 
 fig = px.line(hourly_sales, x='hour', y='revenue', title="Hourly Revenue Trend")
+
 
 st.plotly_chart(fig)
